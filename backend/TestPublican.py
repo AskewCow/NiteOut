d335a58bf6fe316980efecbaf00841e87fdb9306
Emma Wijsman <wijsmane@tcd.ie>
2025-02-13 10:48:42 +0000

    small fixes

--- backend/TestPublican.py
+++ backend/TestPublican.py
@@ -4,10 +4,10 @@ from Publican import Publican
 class TestPublican(unittest.TestCase):
     
     def setUp(self):
-        self.Publican = (self, "Cassidy's", "cassidys@gmail.com", 231, "password", "123 Shoe Lane", 53.349805, -6.26031, 10)
+        self.Publican = Publican("Cassidy's", "cassidys@gmail.com", "231", "password", "123 Shoe Lane", 53.349805, -6.26031, 10)
     
     def test_pub_details(self):
-        details = self.pub.pub_details()
+        details = self.Publican.pub_details()
         self.assertEqual(details["name"], "Cassidy's")
         self.assertEqual(details["email"], "cassidys@gmail.com")
         self.assertEqual(details["ID(verification)"], "231")
@@ -18,12 +18,12 @@ class TestPublican(unittest.TestCase):
         self.assertEqual(details["tables"], 10)
 
     def test_reserve_table(self):
-        result = self.pub.reserve_table(4)
+        result = self.Publican.reserve_table(4)
         self.assertEqual(result, "Table reserved.")
-        self.assertEqual(self.pub.get_tables(), 9)
+        self.assertEqual(self.Publican.get_tables(), 9)
 
     def test_reserve_table_not_enough_tables(self):
-        self.pub = Publican(
+        self.Publican = Publican(
             pub_name="Doyle's", 
             email="doyles@gmail.com", 
             ID="345", 
@@ -33,33 +33,33 @@ class TestPublican(unittest.TestCase):
             ycoord=-8.472, 
             tables=1
         )
-        result = self.pub.reserve_table(10)
+        result = self.Publican.reserve_table(10)
         self.assertEqual(result, "No/not enough tables available.")
-        self.assertEqual(self.pub.get_tables(), 1)
+        self.assertEqual(self.Publican.get_tables(), 1)
 
     def test_cancel_reservation(self):
-        self.pub.reserve_table(4)
-        result = self.pub.cancel_reservation(4)
+        self.Publican.reserve_table(4)
+        result = self.Publican.cancel_reservation(4)
         self.assertEqual(result, "Reservation cancelled")
-        self.assertEqual(self.pub.get_tables(), 10)
+        self.assertEqual(self.Publican.get_tables(), 10)
 
     def test_cancel_reservation_no_tables_reserved(self):
-        result = self.pub.cancel_reservation(4)
+        result = self.Publican.cancel_reservation(4)
         self.assertEqual(result, "Reservation cancelled")
-        self.assertEqual(self.pub.get_tables(), 10)
+        self.assertEqual(self.Publican.get_tables(), 10)
 
     def test_reserve_table_for_multiple_groups(self):
-        self.pub.reserve_table(4)
-        result = self.pub.reserve_table(6)
+        self.Publican.reserve_table(4)
+        result = self.Publican.reserve_table(6)
         self.assertEqual(result, "Table reserved.")
-        self.assertEqual(self.pub.get_tables(), 7)
+        self.assertEqual(self.Publican.get_tables(), 7)
 
     def test_reserve_table_for_small_group(self):
-        result = self.pub.reserve_table(2) 
+        result = self.Publican.reserve_table(2) 
         self.assertEqual(result, "Table reserved.")
-        self.assertEqual(self.pub.get_tables(), 9)
+        self.assertEqual(self.Publican.get_tables(), 9)
 
     def test_reserve_table_with_large_group(self):
-        result = self.pub.reserve_table(12)
+        result = self.Publican.reserve_table(12)
         self.assertEqual(result, "Table reserved.")
-        self.assertEqual(self.pub.get_tables(), 7)
+        self.assertEqual(self.Publican.get_tables(), 7)
